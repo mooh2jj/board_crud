5d9fa8c5c7c557384bddf3543a9725d2612c461a
도성곤 <sg.do@unionplace.co.kr>
2021-07-12 16:15:20 +0900

    jwt 구현 2분 이상 지나면 토큰 만료

---
+++ src/main/java/com/example/board_crud/security/SecurityService.java
@@ -0,0 +1,45 @@
+package com.example.board_crud.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class SecurityService {
+    private static final String SECRET_KEY = "sdfdsjfkldjsklfdjslskskskskkskafdslkldsfjkdsjfdsjlfjdklsfdslf";
+
+    // 로그인 서비스 던질때 같이
+    public String createToken(String subject, long expTime) {
+        if (expTime <= 0) {
+            throw new RuntimeException("만료 시간 0보다 커야합니다.");
+        }
+
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        byte[] secretKeyBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingKey = new SecretKeySpec(secretKeyBytes, signatureAlgorithm.getJcaName());    // key가 만들어짐.
+
+        return Jwts.builder()
+                .setSubject(subject)
+                .signWith(signingKey, signatureAlgorithm)
+                .setExpiration(new Date(System.currentTimeMillis() + expTime))
+                .compact();
+    }
+
+    // 토큰 검증하는 메서드를 boolean으로
+    public String getSubject(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.getSubject();
+    }
+}
